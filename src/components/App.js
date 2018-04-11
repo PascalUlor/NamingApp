6c3064a71ade7dfc0eae823f9ba4b6c1a0151760
Pascal Ulor <pascalulor@yahoo.com>
2018-04-11 03:26:54 +0100

    render state using conuctors. This were used before babelpreset-stage-2

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { test:42 };
+    }
     render() {
         return (
             <div>
             <Header message="Naming Contest" />
             <div>
-        
+                {this.state.test}
             </div>
             </div>
             );
