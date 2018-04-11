c52a0b31b8e29dafdffa2ef63df56ab6604cabc8
Pascal Ulor <pascalulor@yahoo.com>
2018-04-11 03:36:41 +0100

    render state without using constructors since I have babelpreset-stage-2

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Header from './Header';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { test:42 };
-    }
+    state ={
+        pageHeader: "Naming Contests"
+    };
     render() {
         return (
             <div>
-            <Header message="Naming Contest" />
+            <Header message={this.state.pageHeader} />
             <div>
-                {this.state.test}
+               Empty div 
             </div>
             </div>
             );
