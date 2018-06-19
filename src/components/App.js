0280a6a52f6f38a24136c61a0a66ecd9dd801789
Pascal Ulor <pascalulor@yahoo.com>
2018-06-19 13:42:16 +0100

    bug(fix issues with adding contests): contests do not update automatically

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
       });
     }
     componentWillUnmount() {
-      // onPopState(null);
+      onPopState(null);
     }
     fetchContest = (contestId) => {
       pushState(
